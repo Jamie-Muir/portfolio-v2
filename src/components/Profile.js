a4ed300ad17c7bd4c2aa155445b6fdf8b3c7f18e
jamie-muir <jamiemuircs@gmail.com>
2022-12-11 02:07:39 +0000

    media queries

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,6 +8,7 @@ function Profile() {
 			<h1>Jamie Muir</h1>
 			<h2> <span>Front-End</span> Developer </h2>
 			<p>
+				A work in progress. <br /><br />
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
 				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
 				nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
